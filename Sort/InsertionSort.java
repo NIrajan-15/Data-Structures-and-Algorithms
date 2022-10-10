04ef417dbeb06bf2db73ea6114ec0199dbab0e66
Nirajan Sangraula <neerazansangraula14@gmail.com>
2022-10-10 17:36:03 -0500

    Insertion Sort added

---
+++ Sort/InsertionSort.java
@@ -0,0 +1,56 @@
+package Sort;
+import java.util.Arrays;
+
+// Insertion Sort Algorithm
+
+
+/*  
+   time complextiy: 
+      best : O(N) -> If the arrays is in ascending order or (already sorted)
+      worst : O(N^2)-> If the array is in descending order
+   
+   space complexity:
+      O(1) -> no new arrays are required for copying or whatever
+*/
+
+
+//public class InerstionSort
+public class InsertionSort {
+    public static void main(String[] args)
+    {
+        // Array to be sorted
+        int[] arr = {4,5,1,2,3};
+        System.out.println(Arrays.toString(search(arr)));
+    }
+    /* 
+     * Static method search 
+     * Takes integer array arr as paremeter
+     * Returns the same array but sorted
+     */
+    static int[] search(int[] arr)
+    {
+        // To loop until second last element
+        for(int i=0;i<arr.length-1;i++)
+        {
+            // traverse array in reverse to find smaller elements
+            for(int j=i+1;j>0;j--)
+            {
+                
+                // if element is smaller than its previous one then swap them
+                if(arr[j]<arr[j-1])
+                {
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                    
+                }
+                // if element is greater than its previous one then break
+                else{
+                    break;
+                }
+            }
+        }
+        //return the sorted beautiful array
+        return arr;
+    }
+}
